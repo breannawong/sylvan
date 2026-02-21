f54f95567bd05494bf8dae86dd283cf19bb39dbd
Breanna Wong <breannawong12@gmail.com>
2026-02-21 17:30:31 -0500

    trying to fix index.html

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+console.log("ELEVENTY NODE_ENV:", process.env.NODE_ENV);
 const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
